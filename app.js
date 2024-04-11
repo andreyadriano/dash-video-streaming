a496237e2ffa34475c4112a8bc41d87186b10c45
Andrey Adriano <dey2010dey@gmail.com>
2024-04-11 20:12:16 -0300

    add DASH processing script

---
+++ app.js
@@ -0,0 +1,27 @@
+let ffmpegStatic = require('ffmpeg-static')
+const ffmpeg = require('fluent-ffmpeg')
+ffmpeg.setFfmpegPath(ffmpegStatic)
+
+let inputPath = "video.mp4"
+const outputPath = "output_dash/output.mpd"
+
+const scaleOptions = [
+    "scale=720:480",
+]
+
+const videoCodec = "libx264"
+const x264Options = "keyint=24:min-keyint=24:no-scenecut"
+const videoBitRates = ['500k', '1000k', '2000k', '4000k']
+
+ffmpeg()
+    .input(inputPath)
+    .videoFilters(scaleOptions)
+    .videoCodec(videoCodec)
+    .addOption("-x264opts", x264Options)
+    .outputOptions("-b:v", videoBitRates[0])
+    .format('dash')
+    .output(outputPath)
+    .on('end', () => {
+        console.log('DASH Encoding finished!')
+    })
+    .run()
